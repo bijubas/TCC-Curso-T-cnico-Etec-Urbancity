9b443673fc120c7e884398725c5e7d5455e8a600
Biju <bianca.vieira3366@gmail.com>
2026-05-19 23:08:51 -0300

    Add files via upload

---
+++ urbancity_sejacomu/sejacomu.js
@@ -0,0 +1,8 @@
+//nav
+function loginpag() {
+    window.location.href = "../urbancity_login_cadastro/login.html"
+}
+
+function cadastro() {
+    window.location.href = "../urbancity_login_cadastro/cadastro.html"
+}
